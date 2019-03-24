907c210881822ff35c2603fcdda3cd22ac3b577d
Mauro Cicolella <mcicolella@libero.it>
2019-03-24 17:19:19 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Freedomotic Developer Manual'
-copyright = u'2009-2018, Freedomotic Team, http://www.freedomotic.com'
+copyright = u'2009-2019, Freedomotic Team, http://www.freedomotic-iot.com'
 author = u'The Freedomotic Team'
 
 # The version info for the project you're documenting, acts as replacement for
